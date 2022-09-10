99315f532ff416faa09ac0a96fc965abcbcb8c8a
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-09-11 03:52:08 +0900

    add Example app project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by p-x9 on 2022/09/11.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
