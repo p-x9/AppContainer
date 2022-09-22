644178287c766dffa82293d8fba4bd58ab27b8ca
p-x9 <50244599+p-x9@users.noreply.github.com>
2022-09-23 00:43:42 +0900

    remove stroryboard

--- Example/Example/SceneDelegate.swift
+++ Example/Example/SceneDelegate.swift
@@ -17,7 +17,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        setupWindow(with: windowScene)
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -51,3 +52,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+extension SceneDelegate {
+    func setupWindow(with windowScene: UIWindowScene) {
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
+        
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window.rootViewController = navigationController
+        
+        window.makeKeyAndVisible()
+    }
+}
