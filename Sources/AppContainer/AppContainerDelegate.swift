ceca74e961a2b29c694e6af9277596a164d07ee2
p-x9 <50244599+p-x9@users.noreply.github.com>
2023-03-10 22:48:29 +0900

    implemented `delegates` to `AppContainer`

--- Sources/AppContainer/AppContainerDelegate.swift
+++ Sources/AppContainer/AppContainerDelegate.swift
@@ -8,12 +8,12 @@
 
 import Foundation
 
-protocol AppContainerDelegate: AnyObject {
-    func appContainer(_ appContainer: AppContainer, willChangeTo toContainer: Container, from fromContainer: Container)
-    func appContainer(_ appContainer: AppContainer, didChangeTo toContainer: Container, from fromContainer: Container)
+public protocol AppContainerDelegate: AnyObject {
+    func appContainer(_ appContainer: AppContainer, willChangeTo toContainer: Container, from fromContainer: Container?)
+    func appContainer(_ appContainer: AppContainer, didChangeTo toContainer: Container, from fromContainer: Container?)
 }
 
 extension AppContainerDelegate {
-    func appContainer(_ appContainer: AppContainer, willChangeTo toContainer: Container, from fromContainer: Container) {}
-    func appContainer(_ appContainer: AppContainer, didChangeTo toContainer: Container, from fromContainer: Container) {}
+    public func appContainer(_ appContainer: AppContainer, willChangeTo toContainer: Container, from fromContainer: Container?) {}
+    public func appContainer(_ appContainer: AppContainer, didChangeTo toContainer: Container, from fromContainer: Container?) {}
 }
